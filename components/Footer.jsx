cbc4de8227b7c889bba6bb743d3f99c43ee84452
Haris Hummam <95171578+Harum101@users.noreply.github.com>
2024-04-29 18:16:22 +0500

    Added Github Link

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,22 +1,44 @@
 "use client";
 import Link from "next/link";
-import { FaFacebookSquare, FaInstagramSquare, FaLinkedin, FaTwitterSquare } from "react-icons/fa";
+import {
+  FaFacebookSquare,
+  FaGithubSquare,
+  FaInstagramSquare,
+  FaLinkedin,
+  FaTwitterSquare,
+} from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div className="block text-center font-montserrat text-white">
       <p>
-        &copy; Copyright <span className="font-bold animate-pulse">Haris Hummam</span>. All
+        &copy; Copyright{" "}
+        <span className="font-bold animate-pulse">Haris Hummam</span>. All
         Rights Reserved
       </p>
       <div className="flex justify-center items-center space-x-2 m-3">
-        <Link href="https://www.facebook.com/profile.php?id=100005285476593" className="hover:text-gray-800">
+        <Link
+          href="https://github.com/Harum101"
+          className="hover:text-gray-800"
+        >
+          <FaGithubSquare size={30} />
+        </Link>
+        <Link
+          href="https://www.facebook.com/profile.php?id=100005285476593"
+          className="hover:text-gray-800"
+        >
           <FaFacebookSquare size={30} />
         </Link>
-        <Link href="https://twitter.com/HarisHummam1" className="hover:text-gray-800">
+        <Link
+          href="https://twitter.com/HarisHummam1"
+          className="hover:text-gray-800"
+        >
           <FaTwitterSquare size={30} />
         </Link>
-        <Link href="https://www.linkedin.com/in/haris-hummam-a404621ba/" className="hover:text-gray-800">
+        <Link
+          href="https://www.linkedin.com/in/haris-hummam-a404621ba/"
+          className="hover:text-gray-800"
+        >
           <FaLinkedin size={30} />
         </Link>
         <Link href="#" className="hover:text-gray-800">
